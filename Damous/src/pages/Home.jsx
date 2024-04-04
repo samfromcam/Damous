0fcdfa6ab5403a4672d7338425a85ff39d034117
Samnang Nuonsinoeun <samnangnuonsineoun2025@u.northwestern.edu>
2024-04-04 16:20:54 -0500

    v1

---
+++ Damous/src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import Header from '../partials/Header';
+import PageIllustration from '../partials/PageIllustration';
+import HeroHome from '../partials/HeroHome';
+import FeaturesBlocks from '../partials/FeaturesBlocks';
+import FeaturesZigZag from '../partials/FeaturesZigzag';
+import Team from '../partials/Team';
+import Newsletter from '../partials/Newsletter';
+import Footer from '../partials/Footer';
+
+function Home() {
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+      {/*  Site header */}
+      <Header />
+
+      {/*  Page content */}
+      <main className="grow">
+        {/*  Page illustration */}
+        <div className="relative max-w-6xl mx-auto h-0 pointer-events-none" aria-hidden="true">
+          <PageIllustration />
+        </div>
+
+        {/*  Page sections */}
+        <HeroHome />
+        <FeaturesZigZag />
+        <FeaturesBlocks />
+        <Team />
+        <Newsletter />
+      </main>
+
+      {/*  Site footer */}
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
