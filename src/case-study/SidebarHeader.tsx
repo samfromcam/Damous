aa4ab0bdc75e3db7d9ce6a3c4f25f4a1913fb273
Samnang Nuonsinoeun <samnangnuonsineoun2025@u.northwestern.edu>
2024-07-25 11:42:40 -0500

    change the case study

---
+++ src/case-study/SidebarHeader.tsx
@@ -0,0 +1,26 @@
+import logo from '../assets/northwestern_logo.png';
+import './SidebarHeader.css';
+import "@fontsource/poppins/700.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/500.css";
+
+function SidebarHeader() {
+    return (
+        <aside className="sidebar">
+            <div className="sidebar-title">
+                <h1>Civilian Comfort in Underground Urban Transport</h1>
+            </div>
+            <div className='sidebar-logo'>
+                <img src={logo} alt="Northwestern University logo" />
+            </div>
+            <nav className='sidebar-nav'>
+                <a href="#problem">Problem</a>
+                <a href="#solution">Solution</a>
+                <a href="#result">Result</a>
+                <a href="#review">Review</a>
+            </nav>
+        </aside>
+    );
+}
+
+export default SidebarHeader;
